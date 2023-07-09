58a3aa1bc88db98683a5d21b3c643a82ccaea585
linjinbao666 <31817294+linjinbao666@users.noreply.github.com>
2023-07-09 01:56:39 +0000

    use colly

--- main.go
+++ main.go
@@ -1,106 +1,33 @@
 package main
 
 import (
-	"bufio"
-	"encoding/json"
-	"flag"
 	"fmt"
-	"io"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
-	"strings"
-	"time"
-)
 
-const BING_API = "https://bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&nc=1612409408851&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160"
-const BING_URL = "https://bing.com"
-const URL_ATTACH = "&pid=hp&w=384&h=216&rs=1&c=4"
-const IMAGES = "images"
-const README = "README.md"
-
-var result map[string]interface{}
-
-var (
-	filepath string
-	random   bool
+	"github.com/gocolly/colly/v2"
 )
 
 func main() {
-	flag.Parse()
-	c := http.Client{Timeout: time.Duration(1) * time.Second}
-	resp, _ := c.Get(BING_API)
-	defer resp.Body.Close()
-	body, _ := ioutil.ReadAll(resp.Body)
-	err := json.Unmarshal([]byte(body), &result)
-	if err != nil {
-		fmt.Printf("Error %s", err)
-		return
-	}
-	images := result["images"]
-	var text string
-	var enddate string
-	var url string
-	var url2 string
-	var title string
-	var name string
+	// Instantiate default collector
+	c := colly.NewCollector(
+		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+		colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
+	)
 
-	for _, image := range images.([]interface{}) {
-		url = image.(map[string]interface{})["url"].(string)
-		url2 = strings.Split(url, "&")[0]
-		title = image.(map[string]interface{})["title"].(string)
-		enddate = image.(map[string]interface{})["enddate"].(string)
-		text = "\n" + "![" + title + "](" + BING_URL + url2 + URL_ATTACH + ")" + "\n"
-		name = strings.Split(url2, "=")[1]
+	// On every a element which has href attribute call callback
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
 
-		os.MkdirAll(IMAGES+"/"+enddate, os.ModePerm)
-		fmt.Println("url: " + url)
-		fmt.Println("url2: " + url2)
-		URL := BING_URL + url2
-		fileName := name
-		err = download(URL, IMAGES+"/"+enddate+"/"+fileName)
-		if err != nil {
-			log.Fatal(err)
-		}
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
 
-		readme(text)
-	}
-
-}
-
-func readme(text string) {
-	file, err := os.OpenFile(README, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
-	if err != nil {
-		fmt.Println("file open failed", err)
-	}
-	defer file.Close()
-
-	writer := bufio.NewWriter(file)
-	writer.WriteString(text)
-	writer.Flush()
-}
-
-func download(URL, name string) error {
-	fmt.Println(URL)
-	response, _ := http.Get(URL)
-	defer response.Body.Close()
-	if response.StatusCode != 200 {
-		return nil
-	}
-	file, err := os.Create(name)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-	_, err = io.Copy(file, response.Body)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func init() {
-	flag.StringVar(&filepath, "filepath", "README.md", "write to file")
-	flag.BoolVar(&random, "random", false, "if true, random image")
+	// Start scraping on https://hackerspaces.org
+	c.Visit("https://hackerspaces.org/")
 }
